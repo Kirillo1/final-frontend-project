bcb3515f7a06c75e2c13c9f2fe91304f69815456
ghost <kirill.han011@gmail.com>
2024-06-13 14:19:50 +0300

    изменена структура проекта

--- backend/migrations/env.py
+++ backend/migrations/env.py
@@ -1,12 +1,17 @@
+import os
+import sys
+
 from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
+sys.path.append(os.path.join(sys.path[0], 'src'))
 
-from config import DB_HOST, DB_NAME, DB_PORT, DB_PASS, DB_USER
-from models.models import metadata
+from src.config import DB_HOST, DB_NAME, DB_PORT, DB_PASS, DB_USER
+from src.smartphone.models import metadata as metadata_smartphone
+
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -29,7 +34,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = metadata
+target_metadata = [metadata_smartphone]
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
