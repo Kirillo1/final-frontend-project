f836922ebeb8f91e7fb672b32f958a92d034ee20
ghost <kirill.han011@gmail.com>
2024-06-13 12:41:35 +0300

    Добавлены тестовые функции

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,14 +1,31 @@
 from fastapi import FastAPI
+from pydantic import BaseModel, Field
+
+from typing import List
+
+app = FastAPI(title="Mobile Guru")
 
 
-app = FastAPI()
+# @app.get("/", response_model=List[Smartphone])
+# async def get_smartphones():
+#     pass
 
 
-@app.get("/")
-async def get_smartphones():
+@app.get("/smartphone/{smartphone_id}")
+async def get_smartphone(smartphone_id: int):
     pass
 
 
-@app.get("/smartphone")
-async def get_smartphone():
+@app.post("/add_smartphone")
+async def add_smartphone():
+    pass
+
+
+@app.post("/edit_smartphone/{smartphone_id}")
+async def edit_smartphone(smartphone_id: int):
+    pass
+
+
+@app.get("/delete_smartphone/{smartphone_id}")
+async def delete_smartphone(smartphone_id: int):
     pass
