8a1abee9e60fd6eb191fefabe945f4cc98d520c9
ghost <kirill.han011@gmail.com>
2024-06-25 14:29:32 +0300

    Добавлены модели и логика для регистрации и логина пользователя. Требуется доработка.

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,9 +1,26 @@
 from fastapi import FastAPI
 
+from src.users.base_config import auth_backend, fastapi_users
+from src.users.schemas import UserRead, UserCreate
+
 from src.smartphone.router import router as router_smartphone
 
 
 app = FastAPI(title="Mobile Guru")
+
+
+app.include_router(
+    fastapi_users.get_auth_router(auth_backend),
+    prefix="/auth",
+    tags=["Auth"],
+)
+
+
+app.include_router(
+    fastapi_users.get_register_router(UserRead, UserCreate),
+    prefix="/auth",
+    tags=["Auth"],
+)
 
 
 app.include_router(router_smartphone,)
