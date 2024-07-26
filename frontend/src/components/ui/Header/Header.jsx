ddbb0a99664c3d5e22197b701631a5735ebc1c91
ghost <kirill.han011@gmail.com>
2024-07-26 13:21:59 +0300

    Изменен header. Обновлены зависимости.

--- frontend/src/components/ui/Header/Header.jsx
+++ frontend/src/components/ui/Header/Header.jsx
@@ -1,12 +1,41 @@
+import { useState } from 'react'
 import { NavLink, useLocation } from "react-router-dom";
+import KeyboardArrowDownRoundedIcon from '@mui/icons-material/KeyboardArrowDownRounded';
+import CloseRoundedIcon from '@mui/icons-material/CloseRounded';
+import MenuRoundedIcon from '@mui/icons-material/MenuRounded';
+import SmartphoneRoundedIcon from '@mui/icons-material/SmartphoneRounded';
+import HeadphonesBatteryRoundedIcon from '@mui/icons-material/HeadphonesBatteryRounded';
+import {
+    Dialog,
+    DialogPanel,
+    Disclosure,
+    DisclosureButton,
+    DisclosurePanel,
+    Popover,
+    PopoverButton,
+    PopoverGroup,
+    PopoverPanel,
+} from '@headlessui/react'
+
+/** Массив подпунктов меню добавления */
+const products = [
+    { 
+        name: 'Смартфон', 
+        description: 'Добавить смартфон', 
+        href: '#', icon: SmartphoneRoundedIcon 
+    },
+    { 
+        name: 'Аксессуар', 
+        description: 'Добавить аксессуар', 
+        href: '#', icon: HeadphonesBatteryRoundedIcon 
+    },
+]
 
 /** Массив пунктов меню */
 const navItems = [
     { name: "Главная", path: "/" },
     { name: "Смартфоны", path: "/smartphones" },
     { name : "Панель администратора", path: "/admin_panel" },
-    { name: "Добавить смартфон", path: "/add_smartphone" },
-    { name: "Добавить аксессуар", path: "/add_accessory" }
 ];
 
 /**
@@ -15,6 +44,8 @@ const navItems = [
  */
 const Header = () => {
     const location = useLocation();
+
+    const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
 
     /**
      * Определяет, активна ли ссылка.
@@ -29,10 +60,83 @@ const Header = () => {
     };
 
     return (
-        <header className="bg-black fixed top-0 left-0 right-0 z-10 shadow-md shadow-violet-700">
-            <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
-                <div className="relative flex justify-between h-16">
-                    <nav className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
+        <header className="bg-black">
+            <nav aria-label="Global" className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8">
+                <NavLink to="/" className="text-white text-xl flex-shrink-0 flex items-center">
+                    <img
+                        className="block lg:hidden h-8 w-auto"
+                        src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
+                        alt="Workflow"
+                    />
+                    MobileGuru
+                </NavLink>
+                <div className="flex lg:hidden">
+                    <button
+                        type="button"
+                        onClick={() => setMobileMenuOpen(true)}
+                        className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+                    >
+                        <span className="sr-only">Открыть главное меню</span>
+                        <MenuRoundedIcon aria-hidden="true" className="h-6 w-6" />
+                    </button>
+                </div>
+                <PopoverGroup className="hidden lg:flex lg:gap-x-12">
+                    <div className="hidden sm:ml-8 sm:flex sm:space-x-8">
+                        {navItems?.map((item) => (
+                            <NavLink
+                                to={item?.path}
+                                key={item?.path}
+                                className={`text-white inline-flex items-center px-1 pt-1 text-sm ${isActiveLink(item?.path)
+                                    ? "text-violet-500"
+                                    : "hover:text-violet-500"
+                                    }`}
+                            >
+                                {item?.name}
+                            </NavLink>
+                        ))}
+                        <Popover className="relative">
+                            <PopoverButton className="flex items-center gap-x-1 text-sm font-semibold leading-6 text-white">
+                                Добавить
+                                <KeyboardArrowDownRoundedIcon />
+                            </PopoverButton>
+
+                            <PopoverPanel
+                                transition
+                                className="absolute -left-8 top-full z-10 mt-3 w-screen max-w-md overflow-hidden rounded-3xl bg-white shadow-lg ring-1 ring-gray-900/5 transition data-[closed]:translate-y-1 data-[closed]:opacity-0 data-[enter]:duration-200 data-[leave]:duration-150 data-[enter]:ease-out data-[leave]:ease-in"
+                            >
+                                <div className="p-4">
+                                    {products.map((item) => (
+                                        <div
+                                            key={item.name}
+                                            className="group relative flex items-center gap-x-6 rounded-lg p-4 text-sm leading-6 hover:bg-gray-50"
+                                        >
+                                            <div className="flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white">
+                                                <item.icon aria-hidden="true" className="h-6 w-6 text-gray-600 group-hover:text-indigo-600" />
+                                            </div>
+                                            <div className="flex-auto">
+                                                <a href={item.href} className="block font-semibold text-gray-900">
+                                                    {item.name}
+                                                    <span className="absolute inset-0" />
+                                                </a>
+                                                <p className="mt-1 text-gray-600">{item.description}</p>
+                                            </div>
+                                        </div>
+                                    ))}
+                                </div>
+                            </PopoverPanel>
+                        </Popover>
+                    </div>
+                </PopoverGroup>
+                <div className="hidden lg:flex lg:flex-1 lg:justify-end">
+                    <a href="#" className="text-sm font-semibold leading-6 text-white">
+                        Войти<span aria-hidden="true">&rarr;</span>
+                    </a>
+                </div>
+            </nav>
+            <Dialog open={mobileMenuOpen} onClose={setMobileMenuOpen} className="lg:hidden">
+                <div className="fixed inset-0 z-10" />
+                <DialogPanel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+                    <div className="flex items-center justify-between">
                         <NavLink to="/" className="text-white text-xl flex-shrink-0 flex items-center">
                             <img
                                 className="block lg:hidden h-8 w-auto"
@@ -41,42 +145,69 @@ const Header = () => {
                             />
                             MobileGuru
                         </NavLink>
-                        <div className="hidden sm:ml-8 sm:flex sm:space-x-8">
-                            {navItems?.map((item) => (
-                                <NavLink
-                                    to={item?.path}
-                                    key={item?.path}
-                                    className={`text-white inline-flex items-center px-1 pt-1 text-sm ${isActiveLink(item?.path)
-                                        ? "text-violet-500"
-                                        : "hover:text-violet-500"
-                                        }`}
-                                >
-                                    {item?.name}
-                                </NavLink>
-                            ))}
-                        </div>
-                    </nav>
-                    <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
                         <button
                             type="button"
-                            className="bg-transparent p-1 rounded-full text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                            onClick={() => setMobileMenuOpen(false)}
+                            className="-m-2.5 rounded-md p-2.5 text-gray-700"
                         >
-                            <svg
-                                fill="currentColor"
-                                width="24"
-                                height="24"
-                                viewBox="0 0 32 32"
-                                aria-hidden="true"
-                            >
-                                <path d="M17 24H21V28H17zM24 24H28V28H24zM17 17H21V21H17zM24 17H28V21H24z"></path>
-                                <path d="M28,11h-6V7c0-1.7-1.3-3-3-3h-6c-1.7,0-3,1.3-3,3v4H4c-0.6,0-1,0.4-1,1c0,0.1,0,0.1,0,0.2l1.9,12.1c0.1,1,1,1.7,2,1.7H15v-2	H6.9L5.2,13H28V11z M12,7c0-0.6,0.4-1,1-1h6c0.6,0,1,0.4,1,1v4h-8V7z"></path>
-                            </svg>
+                            <span className="sr-only">Закрыть меню</span>
+                            <CloseRoundedIcon aria-hidden="true" className="h-6 w-6" />
                         </button>
                     </div>
-                </div>
-            </div>
+                    <div className="mt-6 flow-root">
+                        <div className="-my-6 divide-y divide-gray-500/10">
+                            <div className="space-y-2 py-6">
+                                <Disclosure as="div" className="-mx-3">
+                                    <DisclosureButton className="group flex w-full items-center justify-between rounded-lg py-2 pl-3 pr-3.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
+                                        Product
+                                        <KeyboardArrowDownRoundedIcon />
+                                    </DisclosureButton>
+                                    <DisclosurePanel className="mt-2 space-y-2">
+                                        {[...products].map((item) => (
+                                            <DisclosureButton
+                                                key={item.name}
+                                                as="a"
+                                                href={item.href}
+                                                className="block rounded-lg py-2 pl-6 pr-3 text-sm font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                                            >
+                                                {item.name}
+                                            </DisclosureButton>
+                                        ))}
+                                    </DisclosurePanel>
+                                </Disclosure>
+                                <a
+                                    href="#"
+                                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                                >
+                                    Features
+                                </a>
+                                <a
+                                    href="#"
+                                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                                >
+                                    Marketplace
+                                </a>
+                                <a
+                                    href="#"
+                                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                                >
+                                    Company
+                                </a>
+                            </div>
+                            <div className="py-6">
+                                <a
+                                    href="#"
+                                    className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                                >
+                                    Войти
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </DialogPanel>
+            </Dialog>
         </header>
-    );
+    )
 };
 
 export default Header;
